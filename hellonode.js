d8fbd7b7eea8609fa6320d5ff03558756cf9b0fc
Jun <idche@qq.com>
2011-11-01 18:11:16 +0800

    hellonode.js

--- hellonode.js
+++ hellonode.js
@@ -6,3 +6,10 @@ http.createServer(function (req, res) {
 	res.end('Hello Node.js\n');
 	
 }).listen(8124);
+
+/*
+	nohup 命令 &
+	netstat -untpl  查看全部进程
+	killall 进程名字 杀死进程
+	kill -9 pid  杀死指定ID进程
+*/
