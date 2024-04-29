a031e032513a041aa3f6ac8acd0756793b8e300b
代军 <lujun.lj@alibaba-inc.com>
2024-04-29 15:16:47 +0800

    demo

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ var game = new Puzzle(
   true
 );
 game.start();
+
+var game2 = new Puzzle(
+  document.getElementById("puzzle2"),
+  4,
+  "https://img.alicdn.com/imgextra/i4/O1CN01eC3Z3t1DR8nRi2guC_!!6000000000212-0-tps-300-300.jpg",
+  true
+);
+game2.start();
